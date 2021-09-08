038feab0dba41f3fc20f2e3c0c86f054ae3c5cc2
Nikki Tebaldi <ntebaldi@umass.edu>
2021-09-08 10:27:18 -0400

    Hydrograph plotting operations

--- validation.py
+++ validation.py
@@ -4,19 +4,28 @@ Created on Wed Aug 25 13:31:36 2021
 
 @author: coss.31
 """
+# Standard imports
+import datetime
 
-def stats(St,Sq,Vt,Vq):
-    import numpy as np
+# Third-party imports
+import matplotlib.pyplot as plt  
+import matplotlib.dates as Pdate
+import numpy as np
+
+
+def stats(St,Sq,Vt,Vq,IDstr,figdir):
+    
+    # figdir="C:/Users/coss.31/OneDrive - The Ohio State University/Documents/PYfun/valid/figs"
     EMPTY=-9999
+    St=np.array(St)
     # first define overlap times and filter timeseres before comparison
     #pull actual Q and t from uniform mostly empty block
     Vt_=Vt[np.logical_not(np.isnan(Vq))] 
     Vq_=Vq[np.logical_not(np.isnan(Vq))]
     #aditionally remove any -9999
     Vt_=Vt_[Vq_>0]
-    Vq_=Vq_[Vq_>0]
-   
-    olt,idV,ids=np.intersect1d(Vt_,St,return_indices=True)
+    Vq_=Vq_[Vq_>0]    
+    olt,idV,ids=np.intersect1d(Vt_,St,return_indices=True)# these are still potential matches, need to filter again per SWOTq algorithim
     #t does not change between groups?
     Vt_=Vt_[idV]
     Vq_=Vq_[idV]
@@ -29,30 +38,37 @@ def stats(St,Sq,Vt,Vq):
     if len(Sq)<10:
         for Grp in Sq:    
             Sq_=Sq[Grp]       
-            Sq_=Sq_[ids]
+            Sq_=Sq_[ids]           
+             
             ## strictly for testing I am converting -999999999999 to an array of posative value
             #Sq_=np.random.uniform(low=0.0, high=300.0,size=len(Sq_))
                 
-            if any(Sq_>EMPTY) and any(Vq_>EMPTY):     
+            if any(Sq_>EMPTY) and any(Vq_>EMPTY):
+                 #SWOT empty removal
+                St_=St[Sq_>0]
+                Sq_=Sq_[Sq_>0]
+                olt,idV,ids=np.intersect1d(Vt_,St_,return_indices=True)
+                Vt_t=Vt_[idV]
+                Vq_t=Vq_[idV]
                
                 # NSE
-                top=np.sum((Sq_-Vq_)**2)
-                bottom=np.sum(Vq_-np.mean(Vq_))**2
+                top=np.sum((Sq_-Vq_t)**2)
+                bottom=np.sum(Vq_t-np.mean(Vq_t))**2
                 NSE=1-(top/bottom)
                 NSEo.append(NSE)
                  #Rsq
-                r=np.corrcoef( Sq_,Vq_)
+                r=np.corrcoef( Sq_,Vq_t)
                 r=r[0,1]
                 Rsq=r**2
                 Rsqo.append(Rsq)
                 #KGE
-                KGE=1-np.sqrt((r-1)**2 + ((np.std( Sq_)/np.std(Vq_))-1)**2 +((np.mean(Sq_)/np.mean(Vq_))-1)**2)  
+                KGE=1-np.sqrt((r-1)**2 + ((np.std( Sq_)/np.std(Vq_t))-1)**2 +((np.mean(Sq_)/np.mean(Vq_t))-1)**2)  
                 KGEo.append(KGE)
                  #n
-                n=len(Vq_)
+                n=len(Vq_t)
                 no.append(n)
                 #RMSE
-                RMSE=np.sqrt((np.sum( Sq_ - Vq_)**2)/n)
+                RMSE=np.sqrt((np.sum( Sq_ - Vq_t)**2)/n)
                 RMSEo.append(RMSE)
             else:
                     NSEo.append(EMPTY)
@@ -69,32 +85,56 @@ def stats(St,Sq,Vt,Vq):
             "n":no[:]      
             }
     else:
+        
         for Grp in Sq:    
             Sq_=Sq[Grp]       
-            Sq_=Sq_[ids]
-            ## strictly for testing I am converting -999999999999 to an array of posative value
-            #Sq_=np.random.uniform(low=0.0, high=300.0,size=len(Sq_))
-                
-            if any(Sq_>EMPTY) and any(Vq_>EMPTY):     
+            Sq_=Sq_[ids]           
+            St__= St[ids]
+            alg=Grp
+            if any(Sq_>EMPTY) and any(Vq_>EMPTY):
+                 #SWOT empty removal
+                St_=St__[Sq_>0]
+                Sq_=Sq_[Sq_>0]
+                olt,idV,ids=np.intersect1d(Vt_,St_,return_indices=True)
+                Vt_t=Vt_[idV]
+                Vq_t=Vq_[idV]
+                #plot and save
+                strDates = []               
+                for day in Vt_t:
+                    date= datetime.date.fromordinal(day)
+                    strDates.append(date)
+                fig = plt.figure()                
+                ax = fig.add_subplot()
+                dates = Pdate.date2num(strDates)
+                ax.plot_date(dates, Vq_t, fmt='-')
+                ax.plot_date(dates, Sq_, fmt='-')               
                
+                ax.set_ylabel('Q (m^3/s)')
+                ax.legend(['Gage',alg])
+                fig.autofmt_xdate()
+                # figname=figdir+'/'+IDstr+alg+'.jpg'
+                figname=f"{figdir}/{IDstr}_{alg}.jpg"
+                fig.savefig(figname)
+                
+    
                 # NSE
-                top=np.sum((Sq_-Vq_)**2)
-                bottom=np.sum(Vq_-np.mean(Vq_))**2
+                top=np.sum((Sq_-Vq_t)**2)
+                bottom=np.sum(Vq_-np.mean(Vq_t))**2
                 NSE=1-(top/bottom)
                 NSEo.append(NSE)
                  #Rsq
-                r=np.corrcoef( Sq_,Vq_)
+                r=np.corrcoef( Sq_,Vq_t)
                 r=r[0,1]
                 Rsq=r**2
                 Rsqo.append(Rsq)
                 #KGE
-                KGE=1-np.sqrt((r-1)**2 + ((np.std( Sq_)/np.std(Vq_))-1)**2 +((np.mean(Sq_)/np.mean(Vq_))-1)**2)  
+                KGE=1-np.sqrt((r-1)**2 + ((np.std( Sq_)/np.std(Vq_t))-1)**2 +((np.mean(Sq_)/np.mean(Vq_t))-1)**2)  
                 KGEo.append(KGE)
                  #n
-                n=len(Vq_)
+                n=len(Vq_t)
                 no.append(n)
                 #RMSE
-                RMSE=np.sqrt((np.sum( Sq_ - Vq_)**2)/n)
+                RMSE=np.sqrt((np.sum( Sq_ - Vq_t)**2)/n)
                 RMSEo.append(RMSE)
             else:
                     NSEo.append(EMPTY)
