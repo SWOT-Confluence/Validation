7a9e05fc5116dfd9d4108f8f0f809e38d225a7c8
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-11-21 10:04:33 -0500

    Fix container name and add job definition tag

--- deploy/terraform/confluence-validation.tf
+++ deploy/terraform/confluence-validation.tf
@@ -5,6 +5,7 @@ resource "aws_batch_job_definition" "generate_batch_jd_validation" {
   container_properties  = <<CONTAINER_PROPERTIES
   {
     "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-validation",
+    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-validation",
     "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
     "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
     "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
@@ -62,5 +63,8 @@ resource "aws_batch_job_definition" "generate_batch_jd_validation" {
   CONTAINER_PROPERTIES
   platform_capabilities = ["FARGATE"]
   propagate_tags        = true
+   retry_strategy {
+    attempts = 3
+  }
   tags = { "job_definition": "${var.prefix}-validation" }
 }
