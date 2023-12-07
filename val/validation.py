c909afc1844d744aaff0c2f7eb89b5c62bd513ce
Steve Coss <coss.31@osu.edu>
2023-12-07 10:46:23 -0500

    Update validation.py

--- val/validation.py
+++ val/validation.py
@@ -13,22 +13,16 @@ import matplotlib.dates as Pdate
 import numpy as np
 
 
-def stats(St,Sq,Vt,Vq,IDstr,figdir):
+def stats(St,Sq,Vt,Vq,IDstr,figdir):  
     
-    # figdir="C:/Users/coss.31/OneDrive - The Ohio State University/Documents/PYfun/valid/figs"
     EMPTY=-9999
-    St=np.array(St)
-    # first define overlap times and filter timeseres before comparison
-    #pull actual Q and t from uniform mostly empty block
+    St=np.array(St)    
+    # first define overlap times and filter timeseres before comparison    
     Vt_=Vt[np.logical_not(np.isnan(Vq))] 
-    Vq_=Vq[np.logical_not(np.isnan(Vq))]
-    #aditionally remove any -9999
-    Vt_=Vt_[Vq_>0]
-    Vq_=Vq_[Vq_>0]    
-    olt,idV,ids=np.intersect1d(Vt_,St,return_indices=True)# these are still potential matches, need to filter again per SWOTq algorithim
-    #t does not change between groups?
-    Vt_=Vt_[idV]
+    Vq_=Vq[np.logical_not(np.isnan(Vq))]      
+    olt,idV,ids=np.intersect1d(Vt_,St,return_indices=True)# these are still potential matches, need to filter again per SWOTq algorithim    
     Vq_=Vq_[idV]
+    Vt_=Vt_[idV]
     # with nans out index overlap times
     NSEo=[]
     Rsqo=[]
@@ -39,23 +33,15 @@ def stats(St,Sq,Vt,Vq,IDstr,figdir):
     rRMSEo=[]
     no=[]
     offkey=[]
-    if len(Sq)<10:
+    if len(Sq)<10:#if fewer than 10 data points, don't plot. That's all this checks/changes
         for Grp in Sq:    
-            Sq_=Sq[Grp]       
-            Sq_=Sq_[ids]           
-            offkey.append(Grp)
-            ## strictly for testing I am converting -999999999999 to an array of posative value
-            #Sq_=np.random.uniform(low=0.0, high=300.0,size=len(Sq_))
+            Sq_=Sq[Grp]     
+            offkey.append(Grp)           
             if any(Sq_>EMPTY) and any(Vq_>EMPTY):
-                 #SWOT empty removal
-                St_=St[Sq_>0]
-                Sq_=Sq_[Sq_>0]
-                olt,idV,ids=np.intersect1d(Vt_,St_,return_indices=True)
-                Vt_t=Vt_[idV]
-                Vq_t=Vq_[idV]
-                St_ = St_[ids]
-                Sq_ = Sq_[ids]
-                
+                #SWOT empty removal
+                filt=np.where((Sq_>0)&(Vq_>0))[0]        
+                Sq_=Sq_[filt]
+                Vq_t=Vq_[filt]          
                 # NSE
                 top=np.sum((Vq_t-Sq_)**2)
                 bottom=np.sum((Vq_t-np.mean(Vq_t))**2)
@@ -105,7 +91,7 @@ def stats(St,Sq,Vt,Vq,IDstr,figdir):
             "nBIAS":nBIASo[:],
             "rRMSE":rRMSEo[:],
             "n":no[:],
-            "t":St
+            "t":St_           
             }
     else:
         
@@ -116,14 +102,11 @@ def stats(St,Sq,Vt,Vq,IDstr,figdir):
             alg=Grp
             offkey.append(Grp)
             if any(Sq_>EMPTY) and any(Vq_>EMPTY):
-                 #SWOT empty removal
-                St_=St__[Sq_>0]
-                Sq_=Sq_[Sq_>0]
-                olt,idV,ids=np.intersect1d(Vt_,St_,return_indices=True)
-                Vt_t=Vt_[idV]
-                Vq_t=Vq_[idV]
-                St_ = St_[ids]
-                Sq_ = Sq_[ids]
+                #SWOT empty removal
+                filt=np.where((Sq_>0)&(Vq_>0))[0]        
+                Sq_=Sq_[filt]
+                Vq_t=Vq_[filt]                
+                Vt_t=Vt_[filt]                        
                 #plot and save
                 strDates = []               
                 for day in Vt_t:
@@ -189,7 +172,7 @@ def stats(St,Sq,Vt,Vq,IDstr,figdir):
             "nBIAS":nBIASo[:],
             "rRMSE":rRMSEo[:],
             "n":no[:],
-            "t":St
+            "t":St           
             }
    
     return  validout
