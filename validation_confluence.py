5d571dbc6f45dc3974c5bc91f0070ec5fa96553b
Steve Coss <coss.31@osu.edu>
2026-08-23 09:31:42 -0400

    Update validation_confluence.py

--- validation_confluence.py
+++ validation_confluence.py
@@ -662,7 +662,7 @@ class ValidationConfluence:
             try:
                 if self.offline_data:
                     print('o before stats')
-                    print(self.data_offline)
+                    print(self.offline_data)
                     data_offline = stats(time, self.offline_data, self.gage_data["qt"], 
                                       self.gage_data["q"], self.gage_data["gid"], str(self.reach_id), 
                                       self.output_dir / "figs")
