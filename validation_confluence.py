20f074ad2e86459ffb69241336a6651a3368f8ad
Steve Coss <coss.31@osu.edu>
2026-09-23 12:33:49 -0400

    Update validation_confluence.py

--- validation_confluence.py
+++ validation_confluence.py
@@ -778,9 +778,9 @@ class ValidationConfluence:
         out.has_validation_o    = 0 if np.where(stats_O["algorithm"]    == "")[0].size == self.NUM_ALGOS_OFFLINE else 1
         out.gage_type = gage_type.upper()
         if MOIno:
-            out.moi_gauge_status= INT_FILL
-            out.moi_gauge_validation= INT_FILL
-            out.moi_gauge_calibration= INT_FILL
+            out.moi_gauge_status= self.INT_FILL
+            out.moi_gauge_validation= self.INT_FILL
+            out.moi_gauge_calibration= self.INT_FILL
             out.moi_gauge_status=self.reach_con_status
             out.moi_gauge_validation=self.reach_con_validation
             out.moi_gauge_calibration=self.reach_con_calibration
